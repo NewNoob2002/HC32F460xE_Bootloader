6d3fe642fae5916ed29582d1d09c4ae841cd14fb
Ted Guo <guptianci@gmail.com>
2026-07-13 10:17:07 +0800

    Add system clock configuration and update peripheral definitions

--- LegacyReference/mcu_config.h
+++ LegacyReference/mcu_config.h
@@ -1,25 +1,16 @@
 #pragma once
 #include <stdint.h>
 
-#define EXAMPLE_PERIPH_WE                                                      \
-  (LL_PERIPH_GPIO | LL_PERIPH_EFM | LL_PERIPH_FCG | LL_PERIPH_PWC_CLK_RMU |    \
-   LL_PERIPH_SRAM)
-#define EXAMPLE_PERIPH_WP (LL_PERIPH_FCG | LL_PERIPH_SRAM)
+#define CONFIG_PERIPH_WE   (LL_PERIPH_GPIO | LL_PERIPH_EFM | LL_PERIPH_FCG | LL_PERIPH_PWC_CLK_RMU | LL_PERIPH_SRAM)
+#define CONFIG_PERIPH_WP   (LL_PERIPH_FCG | LL_PERIPH_SRAM)
 
-#define XTAL_PORT                   (GPIO_PORT_H)
-#define XTAL_PIN                    (GPIO_PIN_00 | GPIO_PIN_01)
+#define XTAL_PORT          (GPIO_PORT_H)
+#define XTAL_PIN           (GPIO_PIN_00 | GPIO_PIN_01)
 
 /*POWER*/
-#define POWER_CONTROL_PIN		PB3
-#define WATCHDOG_FEED_PIN		PA6
-#define WATCHDOG_FEED_TIME	3000
+#define POWER_CONTROL_PIN  PB3
+#define WATCHDOG_FEED_PIN  PA6
+#define WATCHDOG_FEED_TIME 3000
 
 /*Status LED*/
-#define POWER_LED_PIN PC13
-#define CHARGE_LED_PIN PH2
-#define FUNCTION_LED_PIN PB5
-
-typedef struct ledState_t {
-    uint32_t lastToggleTime;
-    uint16_t currentRate;
-} ledState_t;
+#define FUNCTION_LED_PIN   PB5
